a25327c7cd24f0209c30222a140347cf31ea23e0
Ashish <ashish2000patil@gmail.com>
2022-11-06 15:21:13 +0530

    [Ashish]Added: Ability to Find frequency ToBeOrNotToBe UC-1

--- HashTables/Program.cs
+++ HashTables/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +12,21 @@ namespace HashTables
     {
         static void Main(string[] args)
         {
+            //UC-1
             Console.WriteLine("Welcome to Hash Tables");
+            MyMapNode<int, string> hash = new MyMapNode<int, string>(6);
+            hash.Add(1,"To");
+            hash.Add(2, "be");
+            hash.Add(3, "or");
+            hash.Add(4, "not");
+            hash.Add(5, "to");
+            hash.Add(6, "be");
+
+            for (int i = 0; i <= 6; i++)
+            {
+                Console.WriteLine(hash.Get(i));
+            }
+            Console.ReadLine();
         }
     }
 }
