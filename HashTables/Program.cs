6a79eb89e764462e18abc05683e5a7c8f389b186
Ashish <ashish2000patil@gmail.com>
2022-11-06 15:30:04 +0530

    [Ashish]Added: Frequency in Large Paragraph UC-2

--- HashTables/Program.cs
+++ HashTables/Program.cs
@@ -12,19 +12,32 @@ namespace HashTables
     {
         static void Main(string[] args)
         {
-            //UC-1
+            //UC-2
             Console.WriteLine("Welcome to Hash Tables");
             MyMapNode<int, string> hash = new MyMapNode<int, string>(6);
-            hash.Add(1,"To");
-            hash.Add(2, "be");
-            hash.Add(3, "or");
-            hash.Add(4, "not");
-            hash.Add(5, "to");
-            hash.Add(6, "be");
+            hash.Add(1,"Paranoids");
+            hash.Add(2, "are");
+            hash.Add(3, "not");
+            hash.Add(4, "paranoid");
+            hash.Add(5, "because");
+            hash.Add(6, "they");
+            hash.Add(7, "are");
+            hash.Add(8, "paranoid");
+            hash.Add(9, "but");
+            hash.Add(610, "because");
+            hash.Add(10,"they");
+            hash.Add(11, "keep");
+            hash.Add(12, "putting");
+            hash.Add(13, "themselves");
+            hash.Add(14, "deliberately");
+            hash.Add(15, "into");
+            hash.Add(16, "paranoid");
+            hash.Add(17, "avoidable");
+            hash.Add(18, "situation");
 
-            for (int i = 0; i <= 6; i++)
+            for (int i = 0; i <= 18; i++)
             {
-                Console.WriteLine(hash.Get(i));
+                Console.Write(hash.Get(i)+" ");
             }
             Console.ReadLine();
         }
